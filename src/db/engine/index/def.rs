7422ca3912ef7fbd6aec5793c8f4b2aa316ab6d1
Marat K <phpcraftdream@gmail.com>
2026-02-04 19:13:44 +0300

    ....

--- src/db/engine/index/def.rs
+++ src/db/engine/index/def.rs
@@ -1,6 +1,6 @@
 //! Index definition
 //!
-//! Defines a single index with path and uniqueness.
+//! Defines a single index by path.
 
 use serde::{Deserialize, Serialize};
 
@@ -9,25 +9,12 @@ use serde::{Deserialize, Serialize};
 pub struct IndexDef {
     /// Path to indexed field (interned components)
     pub path: Vec<u64>,
-
-    /// Is this a unique index?
-    pub unique: bool,
 }
 
 impl IndexDef {
-    /// Create a non-unique index
+    /// Create an index definition
     pub fn new(path: Vec<u64>) -> Self {
-        Self { path, unique: false }
-    }
-
-    /// Create a unique index
-    pub fn unique(path: Vec<u64>) -> Self {
-        Self { path, unique: true }
-    }
-
-    /// Check if this is a unique index
-    pub fn is_unique(&self) -> bool {
-        self.unique
+        Self { path }
     }
 }
 
@@ -37,20 +24,14 @@ mod tests {
 
     #[test]
     fn test_index_def() {
-        let def1 = IndexDef::new(vec![1, 2]);
-        assert_eq!(def1.path, vec![1, 2]);
-        assert!(!def1.is_unique());
-
-        let def2 = IndexDef::unique(vec![3, 4]);
-        assert_eq!(def2.path, vec![3, 4]);
-        assert!(def2.is_unique());
+        let def = IndexDef::new(vec![1, 2, 3]);
+        assert_eq!(def.path, vec![1, 2, 3]);
     }
 
     #[test]
     fn test_index_def_serialization() {
         let def = IndexDef {
             path: vec![1, 2, 3],
-            unique: true,
         };
 
         let serialized = bincode::serialize(&def).unwrap();
