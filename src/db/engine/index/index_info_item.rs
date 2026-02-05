b488ec6edb342119b5ea68f1a6265102f49d2873
Marat K <phpcraftdream@gmail.com>
2026-02-05 13:50:53 +0300

    ....

--- src/db/engine/index/index_info_item.rs
+++ src/db/engine/index/index_info_item.rs
@@ -6,12 +6,12 @@ use serde::{Deserialize, Serialize};
 
 /// Definition of a single index
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
-pub struct IndexItem {
+pub struct IndexInfoItem {
     /// Path to indexed field (interned components)
     pub path: Vec<u64>,
 }
 
-impl IndexItem {
+impl IndexInfoItem {
     /// Create an index definition
     pub fn new(path: Vec<u64>) -> Self {
         Self { path }
@@ -24,18 +24,18 @@ mod tests {
 
     #[test]
     fn test_index_def() {
-        let def = IndexItem::new(vec![1, 2, 3]);
+        let def = IndexInfoItem::new(vec![1, 2, 3]);
         assert_eq!(def.path, vec![1, 2, 3]);
     }
 
     #[test]
     fn test_index_def_serialization() {
-        let def = IndexItem {
+        let def = IndexInfoItem {
             path: vec![1, 2, 3],
         };
 
         let serialized = bincode::serialize(&def).unwrap();
-        let deserialized: IndexItem = bincode::deserialize(&serialized).unwrap();
+        let deserialized: IndexInfoItem = bincode::deserialize(&serialized).unwrap();
 
         assert_eq!(deserialized, def);
     }
--- src/db/engine/index/mod.rs
+++ src/db/engine/index/mod.rs
@@ -2,6 +2,6 @@
 //!
 //! Provides index configuration, RAM index storage, and global index manager.
 
-pub mod item;
-pub mod target;
-pub mod manager;
+pub mod index_info_item;
+pub mod index_info;
+pub mod table_index_manager;
